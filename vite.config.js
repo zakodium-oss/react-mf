8054bba94ae0b08f59188dcad788bf8b1468b58d
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2023-04-21 18:52:26 +0200

    fix: build-storybook and disable minify
    with another env variable : STORYBOOK_BUILD. Define a really small config with only react() plugin

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import noBundlePlugin from 'vite-plugin-no-bundle';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
+  if (process.env.STORYBOOK_BUILD) {
+    return {
+      plugins: [
+        react(),
+      ],
+    };
+  }
+
   const format = process.env.VITE_BUID_CJS ? 'cjs' : 'es';
   const extension = process.env.VITE_BUID_CJS ? 'cjs' : 'mjs';
 
@@ -12,6 +20,7 @@ export default defineConfig(() => {
     build: {
       target: 'modules',
       outDir: './lib',
+      minify: false,
       emptyOutDir: !process.env.VITE_BUID_CJS,
       lib: {
         entry: './src/index.js',
