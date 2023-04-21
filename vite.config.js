0943dc1ed871280ce729439ed4c0f86323ca48d9
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2023-04-21 10:46:07 +0200

    build: output .mjs and .cjs
    I had to run build for each format because noBundlePlugin don't allow me to manage it in one run.
    
    I also updated the eslint config to use the storybook recommended.

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,26 @@ import { defineConfig } from 'vite';
 import noBundlePlugin from 'vite-plugin-no-bundle';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  appType: 'custom',
-  build: {
-    target: 'modules',
-    outDir: './lib',
-    lib: {
-      entry: './src/index.js',
-      formats: ['cjs', 'es'],
+export default defineConfig(() => {
+  const format = process.env.VITE_BUID_CJS ? 'cjs' : 'es';
+  const extension = process.env.VITE_BUID_CJS ? 'cjs' : 'mjs';
+
+  return {
+    appType: 'custom',
+    build: {
+      target: 'modules',
+      outDir: './lib',
+      emptyOutDir: !process.env.VITE_BUID_CJS,
+      lib: {
+        entry: './src/index.js',
+        formats: [format],
+      },
     },
-  },
-  plugins: [
-    noBundlePlugin({
-      fileNames: '[name].[format].js',
-    }),
-    react(),
-  ],
+    plugins: [
+      noBundlePlugin({
+        fileNames: `[name].${extension}`,
+      }),
+      react(),
+    ],
+  };
 });
